52c903a6c0559bdc1121e7940a281049820c72ba
Kevin Glinski <kevin.glinski@inin.com>
2017-11-28 13:03:33 -0500

    bug fixes

--- index.js
+++ index.js
@@ -1,145 +1,30 @@
 #!/usr/bin/env node
-
 const fs = require('fs');
 const request = require('request');
 const handlebars = require('./lib/handlebars');
 const getModelName = require('./lib/getModelName');
-
+const lib = require('./lib');
 const args = require('args');
 
 args
   .option('swaggerurl', 'The url to download swagger from')
-  .option('showexamples', 'true to show code examples', false)
+  .option('showexamples', 'true to show code examples')
   .option('templatedir', 'The absolute path the template directory (omit if using default templates)')
   .option('category', 'Category to process (omit processes all)')
   .option('output', 'Directory if processing all files or file location if processing a single category')
 
 
 const DEFAULT_CATEGORY = 'default';
-const flags = args.parse(process.argv)
+const flags = args.parse(process.argv);
 
-let swagger = null;
-var categories = {};
+var swagger = null;
 
-let templateDirectory = __dirname + "/templates";
+var templateDirectory = __dirname + "/templates";
 if(flags.templatedir){
     templateDirectory = flags.templatedir;
 }
 
-function processSwagger(){
 
-    if(!swagger.tags || swagger.tags.length === 0){
-        swagger.tags = [
-            {
-                name: DEFAULT_CATEGORY,
-                description: ""
-            }
-        ]
-    }
-
-    for(let x=0; x< swagger.tags.length; x++){
-        categories[swagger.tags[x].name] = {
-            name: swagger.tags[x].name,
-            description: swagger.tags[x]["description"],
-            operations: [],
-            models:{}
-        };
-    }
-
-    function addToSchemaMap(map, category, schemaName, level){
-        if(level > 3){
-            return;
-        }
-        swagger.definitions[schemaName].modelname = schemaName;
-
-        map[schemaName] = swagger.definitions[schemaName];
-
-        if(swagger.definitions[schemaName].properties){
-            let propertyNames = Object.keys(swagger.definitions[schemaName].properties);
-            propertyNames.forEach((propertyName) =>{
-                let property = swagger.definitions[schemaName].properties[propertyName];
-
-                let modelName = getModelName(property);
-
-                if(modelName){
-                    addToSchemaMap(map, category, modelName, level + 1);
-                }
-            });
-        }
-
-    }
-
-    for(let p=0; p < Object.keys(swagger.paths).length; p++){
-        let path = Object.keys(swagger.paths)[p];
-        let pathData = swagger.paths[path];
-
-        for(let d=0;d< Object.keys(pathData).length; d++){
-            let pathOperation = Object.keys(pathData)[d];
-            let pathOperationData = pathData[pathOperation];
-
-            if(!pathOperationData.operationId){
-                pathOperationData.operationId = path.replace(/\//g, "");
-            }
-
-            pathOperationData.method = pathOperation.toUpperCase();
-            pathOperationData.uri = path;
-
-            if(pathOperationData.responses['200']){
-                let returnType = getModelName(pathOperationData.responses['200']);
-
-                if(returnType){
-                    pathOperationData.returnType = returnType;
-                }
-            }
-
-            if(!pathOperationData.tags || pathOperationData.tags.length === 0){
-                pathOperationData.tags = [DEFAULT_CATEGORY];
-            }
-
-            for(let t=0; t < pathOperationData.tags.length; t++){
-                categories[pathOperationData.tags[t]].operations.push(pathOperationData);
-
-                for(let x=0; x<pathOperationData.parameters.length; x++){
-                    let param = pathOperationData.parameters[x];
-
-
-                    let schemaName = getModelName(param);
-
-                    if(schemaName){
-                        addToSchemaMap(categories[pathOperationData.tags[t]].models, pathOperationData.tags[t], schemaName, 0);
-                    }
-                }
-
-
-                for(let x=0; x< Object.keys(pathOperationData.responses).length; x++){
-                    let key = Object.keys(pathOperationData.responses)[x];
-                    let response = pathOperationData.responses[key];
-
-                    let schemaName = getModelName(response);
-
-                    if(schemaName){
-                        addToSchemaMap(categories[pathOperationData.tags[t]].models,pathOperationData.tags[t], schemaName, 0);
-                    }
-                }
-            }
-        }
-    }
-
-    for(let x=0; x< swagger.tags.length; x++){
-        categories[swagger.tags[x].name].operations.sort((a,b)=>{
-            if(a.uri == b.uri){
-                return a.method.localeCompare(b.method);
-            }
-
-            return a.uri.localeCompare(b.uri);
-        });
-    }
-
-    function generateCategory(categoryName){
-        return template(categories[categoryName]);
-    }
-
-}
 // var swaggerfile = fs.readFileSync(process.argv[2], 'utf8');
 // var swagger = JSON.parse(swaggerfile);
 
@@ -150,26 +35,25 @@ request.get({
     swagger = data;
 
     if(flags.showexamples){
-        handlebars.setShowExamples(flags.showexamples);
+        console.log("Show examples ?" + flags.showexamples);
+        lib.setShowExamples(flags.showexamples);
     }
-    handlebars.setup(templateDirectory, swagger);
-    processSwagger(swagger);
+
+    var categories = lib.setup(swagger, templateDirectory);
 
     if(flags.category){
-        let tag = flags.category;
-        fs.writeFileSync(flags.output, handlebars.execute(categories[tag]));
+        var tag = flags.category;
+        fs.writeFileSync(flags.output, lib.getBody(categories[tag]));
 
     }else{
-        for(let x=0; x< Object.keys(categories).length; x++){
+        for(var x=0; x< Object.keys(categories).length; x++){
 
-            let tag = Object.keys(categories)[x];
-            let category = categories[tag];
+            var tag = Object.keys(categories)[x];
+            var category = categories[tag];
 
             if(category.operations.length > 0){
-                fs.writeFileSync(flags.output + '/_'+ tag.toLowerCase().replace(/ /g, "") +'.html.erb', handlebars.execute(categories[tag]));
+                fs.writeFileSync(flags.output + '/_'+ tag.toLowerCase().replace(/ /g, "") +'.html.erb', lib.getBody(categories[tag]));
             }
-
-
         }
 
     }
---
+++ lib.js
@@ -0,0 +1,154 @@
+var handlebars = require('./lib/handlebars');
+const getModelName = require('./lib/getModelName');
+var categories = {};
+
+function processSwagger(){
+
+    if(!swagger.tags || swagger.tags.length === 0){
+        swagger.tags = [
+            {
+                name: DEFAULT_CATEGORY,
+                description: ""
+            }
+        ]
+    }
+
+    for(var x=0; x< swagger.tags.length; x++){
+        categories[swagger.tags[x].name] = {
+            name: swagger.tags[x].name,
+            description: swagger.tags[x]["description"],
+            operations: [],
+            models:{}
+        };
+    }
+
+    function addToSchemaMap(map, category, schemaName, level){
+        if(level > 3){
+            return;
+        }
+        swagger.definitions[schemaName].modelname = schemaName;
+
+        map[schemaName] = swagger.definitions[schemaName];
+
+        if(swagger.definitions[schemaName].properties){
+            var propertyNames = Object.keys(swagger.definitions[schemaName].properties);
+            propertyNames.forEach((propertyName) =>{
+                var property = swagger.definitions[schemaName].properties[propertyName];
+
+                var modelName = getModelName(property);
+
+                if(modelName){
+                    addToSchemaMap(map, category, modelName, level + 1);
+                }
+            });
+        }
+
+    }
+
+    for(var p=0; p < Object.keys(swagger.paths).length; p++){
+        var path = Object.keys(swagger.paths)[p];
+        var pathData = swagger.paths[path];
+
+        for(var d=0;d< Object.keys(pathData).length; d++){
+            var pathOperation = Object.keys(pathData)[d];
+            var pathOperationData = pathData[pathOperation];
+
+            if(!pathOperationData.operationId){
+                pathOperationData.operationId = path.replace(/\//g, "");
+            }
+
+            pathOperationData.method = pathOperation.toUpperCase();
+            pathOperationData.uri = path;
+
+            if(pathOperationData.responses['200']){
+                var returnType = getModelName(pathOperationData.responses['200']);
+
+                if(returnType){
+                    pathOperationData.returnType = returnType;
+                }
+            }
+
+            if(!pathOperationData.tags || pathOperationData.tags.length === 0){
+                pathOperationData.tags = [DEFAULT_CATEGORY];
+            }
+
+            for(var t=0; t < pathOperationData.tags.length; t++){
+                categories[pathOperationData.tags[t]].operations.push(pathOperationData);
+
+                for(var x=0; x<pathOperationData.parameters.length; x++){
+                    var param = pathOperationData.parameters[x];
+
+
+                    var schemaName = getModelName(param);
+
+                    if(schemaName){
+                        addToSchemaMap(categories[pathOperationData.tags[t]].models, pathOperationData.tags[t], schemaName, 0);
+                    }
+                }
+
+                pathOperationData.parameters.sort(function(a,b){
+                    if(a.in !== b.in){
+                        if(a.in === 'body'){
+                            return 1
+                        }else if(b.in === 'body'){
+                            return -1
+                        }
+
+                        return a.in.localeCompare(b.in);
+                    }
+
+                    return a.name.localeCompare(b.name);
+                });
+
+                for(var x=0; x< Object.keys(pathOperationData.responses).length; x++){
+                    var key = Object.keys(pathOperationData.responses)[x];
+                    var response = pathOperationData.responses[key];
+
+                    var schemaName = getModelName(response);
+
+                    if(schemaName){
+                        addToSchemaMap(categories[pathOperationData.tags[t]].models,pathOperationData.tags[t], schemaName, 0);
+                    }
+                }
+            }
+        }
+    }
+
+    for(var x=0; x< swagger.tags.length; x++){
+        categories[swagger.tags[x].name].operations.sort((a,b)=>{
+            if(a.uri == b.uri){
+                return a.method.localeCompare(b.method);
+            }
+
+            return a.uri.localeCompare(b.uri);
+        });
+    }
+
+    function generateCategory(categoryName){
+        return template(categories[categoryName]);
+    }
+
+}
+
+module.exports = {
+    setShowExamples(show){
+        handlebars.setShowExamples(show);
+    },
+    setup(swaggerJson, templateDir){
+
+        if(!templateDir){
+            templateDir = __dirname + '/templates';
+        }
+
+        handlebars.setup(templateDir, swaggerJson);
+
+        swagger = swaggerJson;
+        processSwagger();
+
+        return categories;
+
+    },
+    getBody(category){
+        return handlebars.execute(category);
+    }
+}
